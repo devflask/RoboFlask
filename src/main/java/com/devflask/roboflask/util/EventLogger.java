436d8d14425ffedd194bd5045180b301f2f58a4e
dehys <lani@dehys.com>
2021-03-27 18:42:18 +0100

    testing

--- src/main/java/com/devflask/roboflask/util/EventLogger.java
+++ src/main/java/com/devflask/roboflask/util/EventLogger.java
@@ -1,21 +1,37 @@
 package com.devflask.roboflask.util;
 
 import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleRemoveEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageDeleteEvent;
+
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
 
 public class EventLogger extends ListenerAdapter {
 
-    @Override
-    public void onGenericEvent(@NotNull GenericEvent event) {
-        GuildMessageReceivedEvent gmre = null;
+    public void onGenericEvent(GenericEvent e){
 
-        if (event instanceof GuildMessageReceivedEvent)  gmre = (GuildMessageReceivedEvent) event;
+        Object o =
+            e instanceof GuildMessageDeleteEvent ? log(e, "GuildMessageDeleteEvent was fired") :
+            e instanceof GuildMemberJoinEvent ? log(e, "GuildMemberJoinEvent was fired") :
+            e instanceof GuildMemberRemoveEvent ? log(e, "GuildMemberRemoveEvent was fired") :
+            e instanceof GuildMemberRoleRemoveEvent ? log(e, "GuildMemberRoleRemoveEvent was fired") :
+            e instanceof GuildMemberRoleAddEvent ? log(e, "GuildMemberRoleAddEvent was fired") :
+        null;
 
-        if (gmre == null){
-            event.getJDA().getGuildById("298480981441118208").getTextChannelById("807733786028015627")
-                    .sendMessage("EVENT: "+event.getClass()).queue();
-        }
     }
+
+    private Object log(Object ... o){
+        GenericEvent e = (GenericEvent) o[0];
+
+        /*TEMP*/ String message = (String) o[1];
+
+        e.getJDA().getGuildById("298480981441118208").getTextChannelById("807733786028015627")
+                .sendMessage(message).queue();
+
+        return null;
+    }
+
 }
